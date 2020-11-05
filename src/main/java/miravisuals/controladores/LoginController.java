3d9d4ad7d936d83422c78608120d5e634ef08ca2
HYBANPC\TFG <ivan.mgarcia125@gmail.com>
2020-11-05 19:05:42 +0100

    Login y Registro

---
+++ src/main/java/miravisuals/controladores/LoginController.java
@@ -0,0 +1,36 @@
+package miravisuals.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import miravisuals.modelo.Usuario;
+import miravisuals.servicios.UsuarioServicio;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	UsuarioServicio usuarioServicio;
+
+	@GetMapping("/")
+	public String welcome() {
+		return "redirect:/public/";
+	}
+	
+
+	@GetMapping("/auth/login")
+	public String login(Model model) {
+		model.addAttribute("usuario", new Usuario());
+		return "login";
+	}
+
+	@PostMapping("/auth/register")
+	public String register(@ModelAttribute Usuario usuario) {
+		usuarioServicio.registrar(usuario);
+		return "redirect:/auth/login";
+	}
+}
