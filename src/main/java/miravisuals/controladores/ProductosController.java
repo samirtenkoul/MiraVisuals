4fbefd5a93231d40a3134d27ec58d62204757297
HYBANPC\TFG <ivan.mgarcia125@gmail.com>
2020-11-12 13:34:07 +0100

    Creacion de mis productos

---
+++ src/main/java/miravisuals/controladores/ProductosController.java
@@ -0,0 +1,129 @@
+package miravisuals.controladores;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import miravisuals.modelo.Producto;
+import miravisuals.modelo.Usuario;
+import miravisuals.servicios.ProductoServicio;
+import miravisuals.servicios.UsuarioServicio;
+
+@Controller
+@RequestMapping("/app")
+public class ProductosController {
+
+	@Autowired
+	ProductoServicio productos;
+	@Autowired
+	UsuarioServicio usuarios;
+
+	private Usuario usuario;
+
+	@ModelAttribute("misproductos")
+	public List<Producto> misProductos() {
+
+		String email = SecurityContextHolder.getContext().getAuthentication().getName();
+		usuario = usuarios.buscarPorEmail(email);
+
+		return productos.productosDeUnPropietario(usuario);
+	}
+
+	@GetMapping("/misproductos")
+	public String listado(Model model, @RequestParam(name = "q", required = false) String query) {
+
+		if (query != null) {
+
+			model.addAttribute("misproductos", productos.buscarMisProductos(query, usuario));
+		}
+
+		return "app/producto/lista";
+	}
+
+	@GetMapping("/misproductos/{id}/eliminar")
+	public String eliminar(@PathVariable Long id) {
+
+		Producto p = productos.findById(id);
+		if (p.getCompra() == null) {
+
+			productos.borrar(p.getId());
+		}
+
+		return "redirect:/app/misproductos";
+	}
+
+	@GetMapping("/producto/nuevo")
+	public String nuevo(Model model) {
+		
+		model.addAttribute("producto", new Producto());
+		
+		return "app/producto/form";
+	}
+	
+	@PostMapping("/producto/nuevo/submit")
+	public String nuevoSubmit(@ModelAttribute Producto producto) {
+		
+		producto.setPropietario(usuario);
+		productos.insertar(producto);
+		
+		return "redirect:/app/misproductos";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
